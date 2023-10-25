bf5162e7486713bac0038e22a085c8e8260f9ef9
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-25 14:26:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Övning_2023_10_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
