9dd8f9d827d36d91128d849a90117344bde8b393
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-25 15:24:18 +0200

    Lagt till i tiger

--- Tiger.cs
+++ Tiger.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Övning_2023_10_25
 {
-    internal class Tiger : Djur
+    internal class Tiger: Djur
     {
+        public string _origin;
+        public Tiger(string Name, int Age, bool IfMagical, string Origin): base(Name, Age, IfMagical)
+        {
+            _origin = Origin;
+        }
         public override void Ljud()
         {
             Console.WriteLine("Tigern låter GRRR!");
